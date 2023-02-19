f28f8798f14d8483513417fb5e0cfc3b74a209a6
Andreas <andreasoh_7@hotmail.com>
2023-02-19 14:27:47 +0100

    twitter css rettelser

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   mode: "jit",
   content: ["../**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "twitterGreen": "#00ba7c",
+        "twitterDarkGreen" : "#009362",
+        "twitterDarkMode" : "#15202b",
+        "twitterLessDark" : "#1e2732", 
+        "twitterLightDark" : "#273340",
+      },
+    },
   },
   plugins: [],
 }
